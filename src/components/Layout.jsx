6fc5ffa56ed191c61fbf8af187094b810e820d7e
Andrey <andrew.saltul@gmail.com>
2024-11-07 23:06:48 -0500

    Added top panel

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 export default function Layout({ children }) {
   return (
-    <div style={{ display: "flex" }}>
-      <Sidebar />
-      <div style={{ marginLeft: "250px", padding: "20px", width: "100%" }}>
-        {children}
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ marginLeft: "250px", padding: "20px", width: "100%" }}>
+          {children}
+        </div>
       </div>
     </div>
   );
