6de7ed688b8b61c1d74669b7bdab7252da8d2e86
Andrey <andrew.saltul@gmail.com>
2024-12-30 23:11:45 -0500

    added EditBusComponent

---
+++ src/components/SchoolBusFront/DashCameraComponent.jsx
@@ -0,0 +1,18 @@
+export default function DashCameraComponent() {
+  return (
+    <div>
+      <h1>Dash cameras</h1>
+      <div>
+        <table className="table">
+          <thead>
+            <td>NAME</td>
+            <td>DRID</td>
+            <td>IMEI</td>
+            <td>SIM CARD NUMBER</td>
+            <td>SIM CARD HISTORY</td>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
+}
